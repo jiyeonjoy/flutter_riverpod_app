fc9eea2de32aaaaed052ef776a8b939b56dfb9d3
최지연 <choi02647@naver.com>
2023-04-19 13:40:42 +0900

    added Marvel app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter_riverpod_app/counter/counter_app.dart';
+import 'package:flutter_riverpod_app/marvel/marvel_app.dart';
 
 void main() {
-  runCounterApp();
+  runMarvelApp();
 }
